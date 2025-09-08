d4f79bd659033201ac9ee0e4b55ecb3a66535cd0
Ismail <ismael.dirieh@gmail.com>
2025-09-08 15:45:21 +0300

    update todo list without JS

---
+++ js-projects/New-projects/Todo/todo.js
@@ -0,0 +1,12 @@
+const userInput = document.getElementById('user-input')
+const addBtn = document.getElementById('add')
+
+
+addBtn.addEventListener('click', () => {
+    console.log(userInput.value)
+})
+
+// TODO: 1- addTask Func 
+// TODO: 2- delTask Func 
+// TODO: 1- completeTask Func 
+
