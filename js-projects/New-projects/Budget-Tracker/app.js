2ddffd35bedea0b7b0da2680b343134d6f32ddc5
Ismail <ismael.dirieh@gmail.com>
2025-11-13 10:28:06 +0300

    update Budget Tracker #5

--- js-projects/New-projects/Budget-Tracker/app.js
+++ js-projects/New-projects/Budget-Tracker/app.js
@@ -1,22 +1,19 @@
 const addBtn = document.getElementById('add-btn');
 const mainContainer = document.querySelectorAll('.container')[1];
+const amount = document.querySelector('input[type=number]');
+const balance = document.getElementById('balance');
+const transaction = document.querySelector('input[type=text]');  
+const type = document.querySelector('.type-select'); 
+
 
 function addTransaction() {
-    console.log('a');
-    const transaction = document.querySelector('input[type=text]');
-    const amount = document.querySelector('input[type=number]');
-    const type = document.querySelector('.type-select');
-    
-    
-    if(!amount.value) alert('Invalid Number')
-    
     const container = document.createElement('div');
-    const p = document.createElement('p');
+    const descriptionElement = document.createElement('p');
     const transValue = document.createTextNode(transaction.value);
-    p.appendChild(transValue)
+    descriptionElement.appendChild(transValue)
 
     container.classList.add('entry');
-    container.appendChild(p)
+    container.appendChild(descriptionElement)
     
 
     // Transcation Type
@@ -27,8 +24,60 @@ function addTransaction() {
         container.classList.add('expense')
     }
 
+    // Add the Amount
 
-    mainContainer.appendChild(container)
+    if(!amount.value) alert('Invalid Number');
+
+    const amountContainer = document.createElement('div');
+    const amountElement = document.createElement('p');
+    const amountValue = document.createTextNode(`$${amount.value}`);
+    amountElement.appendChild(amountValue);
+
+    amountElement.classList.add('amount');
+    amountContainer.appendChild(amountElement);
+
+    // Delete Button
+    const delButton = document.createElement('button');
+    const delButtonText = document.createTextNode('Delete');
+    delButton.appendChild(delButtonText);
+    delButton.setAttribute('onclick', 'deleteThis(this)')
+    
+    amountContainer.appendChild(delButton)
+
+
+    // Appending Elements
+    container.appendChild(amountContainer)
+    mainContainer.appendChild(container);
+
+    // Updating the Balance
+    balanceTotal();
+
+    transaction.value = "";
+    amount.value = "";
 }
 
-addBtn.addEventListener('click', addTransaction)
+addBtn.addEventListener('click', addTransaction);
+
+
+// Sum the Balance
+
+let totalBalance = 0;
+function balanceTotal() {
+    
+    
+
+    if(type.value == 'income') {
+        totalBalance += +amount.value;
+    }else {
+        totalBalance -= +amount.value;
+    }
+
+    balance.innerText = totalBalance;
+}
+
+// Delete Transaction
+
+function deleteThis(e) {
+    const mainElement = e.parentElement.parentElement;
+    mainElement.remove();
+}
