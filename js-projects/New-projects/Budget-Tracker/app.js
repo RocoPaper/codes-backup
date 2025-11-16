e63b4bef4acd0f65f6ffe241080f79697ecc655b
Ismail <ismael.dirieh@gmail.com>
2025-11-16 15:20:44 +0300

    update Budget Tracker #6

--- js-projects/New-projects/Budget-Tracker/app.js
+++ js-projects/New-projects/Budget-Tracker/app.js
@@ -6,6 +6,14 @@ const transaction = document.querySelector('input[type=text]');
 const type = document.querySelector('.type-select'); 
 
 
+
+function formatCurrency(value) {
+    // return '$' + parseFloat(value).toFixed(2);
+    const options2 = { style: "currency", currency: "USD" };
+    // return '$' + new Intl.NumberFormat(locale).format(value);
+    return new Intl.NumberFormat("en-US", options2).format(value);
+}
+
 function addTransaction() {
     const container = document.createElement('div');
     const descriptionElement = document.createElement('p');
@@ -26,13 +34,17 @@ function addTransaction() {
 
     // Add the Amount
 
-    if(!amount.value) alert('Invalid Number');
+    if(!amount.value){
+        alert('Invalid Number');
+        return;
+    } ;
 
     const amountContainer = document.createElement('div');
     const amountElement = document.createElement('p');
     const amountValue = document.createTextNode(`$${amount.value}`);
     amountElement.appendChild(amountValue);
 
+    amountContainer.classList.add('amount-container');
     amountElement.classList.add('amount');
     amountContainer.appendChild(amountElement);
 
@@ -50,7 +62,7 @@ function addTransaction() {
     mainContainer.appendChild(container);
 
     // Updating the Balance
-    balanceTotal();
+    updateBalance();
 
     transaction.value = "";
     amount.value = "";
@@ -62,22 +74,50 @@ addBtn.addEventListener('click', addTransaction);
 // Sum the Balance
 
 let totalBalance = 0;
-function balanceTotal() {
-    
-    
-
+function updateBalance() {  
     if(type.value == 'income') {
         totalBalance += +amount.value;
     }else {
         totalBalance -= +amount.value;
     }
 
-    balance.innerText = totalBalance;
+    balance.innerText = formatCurrency(totalBalance);
 }
 
 // Delete Transaction
 
 function deleteThis(e) {
     const mainElement = e.parentElement.parentElement;
+    // const theAmount = document.querySelector('.amount');
+    const theAmount = +(e.previousElementSibling.innerText).replace('$', '');
+    console.log(theAmount)
+    removedTransactoin(theAmount)
     mainElement.remove();
 }
+
+
+//Todo: update the amount after removing the Income and Expenses
+
+function removedTransactoin(amount) {  
+    totalBalance += amount
+    
+    balance.innerText = formatCurrency(totalBalance);
+}
+
+// const tables = ['man', 'do', 're', 'fo'];
+
+// window.localStorage.setItem('tables', JSON.stringify(tables))
+
+// console.log(localStorage.getItem('tables'))
+// console.log(JSON.parse(localStorage.getItem('tables')))
+
+
+// function formatNumberWithCommas(numberToFormat) {
+//   return new Intl.NumberFormat(locale).format(numberToFormat);
+// }
+
+
+// Click when press Enter
+amount.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') addBtn.click()
+})
